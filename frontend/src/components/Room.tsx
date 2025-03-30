cf2b478e46da79a9d99305da873638a37a24132d
aryan2574 <ayush.aryan94@yahoo.com>
2025-03-30 15:56:31 +0200

    Implemented user and room logic

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,8 +1,11 @@
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Room = () => {
   const [searchParams] = useSearchParams();
   const name = searchParams.get("name");
+
+  useEffect(() => {}, [name]);
 
   return <div>Hi {name}!</div>;
 };
