917d431ca2caf9becfd4304d7ffeb35d614f4710
aryan2574 <ayush.aryan94@yahoo.com>
2025-04-03 18:19:51 +0200

    Updated App files and bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Landing from "./components/Landing";
-import "./App.css";
-import Room from "./components/Room";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from './components/Landing';
+import { Room } from './components/Room';
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/room" element={<Room />} />
         <Route path="/" element={<Landing />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
