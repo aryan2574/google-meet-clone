91157450a570637a327badfb9905f62b747dbef6
aryan2574 <ayush.aryan94@yahoo.com>
2025-04-06 13:17:03 +0200

    Added User Manager

--- backend/src/managers/UserManager.ts
+++ backend/src/managers/UserManager.ts
@@ -1,8 +1,6 @@
 import { Socket } from "socket.io";
 import { RoomManager } from "./RoomManager";
 
-let GLOBAL_ROOM_ID = 0;
-
 export interface User {
   socket: Socket;
   name: string;
@@ -20,45 +18,57 @@ export class UserManager {
   }
 
   addUser(name: string, socket: Socket) {
-    this.users.push({
-      name,
-      socket,
-    });
+    this.users.push({ name, socket });
     this.queue.push(socket.id);
-    socket.send("lobby");
+
+    socket.emit("lobby");
     this.clearQueue();
     this.initHandlers(socket);
   }
 
   removeUser(socketId: string) {
-    const user = this.users.find((x) => x.socket.id === socketId);
     this.users = this.users.filter((x) => x.socket.id !== socketId);
-    this.queue = this.queue.filter((x) => x === socketId);
+    this.queue = this.queue.filter((x) => x !== socketId);
   }
 
   clearQueue() {
-    if (this.queue.length < 2) {
-      return;
-    }
+    if (this.queue.length < 2) return;
 
-    const user1 = this.users.find((x) => x.socket.id === this.queue.pop());
-    const user2 = this.users.find((x) => x.socket.id === this.queue.pop());
+    const user1 = this.users.find((x) => x.socket.id === this.queue.shift());
+    const user2 = this.users.find((x) => x.socket.id === this.queue.shift());
 
-    if (!user1 || !user2) {
-      return;
-    }
+    if (!user1 || !user2) return;
 
-    const room = this.roomManager.createRoom(user1, user2);
+    this.roomManager.createRoom(user1, user2);
     this.clearQueue();
   }
 
   initHandlers(socket: Socket) {
     socket.on("offer", ({ sdp, roomId }: { sdp: string; roomId: string }) => {
-      this.roomManager.onOffer(roomId, sdp);
+      this.roomManager.onOffer(roomId, sdp, socket.id);
     });
 
     socket.on("answer", ({ sdp, roomId }: { sdp: string; roomId: string }) => {
-      this.roomManager.onAnswer(roomId, sdp);
+      this.roomManager.onAnswer(roomId, sdp, socket.id);
+    });
+
+    socket.on(
+      "ice-candidate",
+      ({
+        roomId,
+        candidate,
+        type,
+      }: {
+        roomId: string;
+        candidate: any;
+        type: "sender" | "receiver";
+      }) => {
+        this.roomManager.onIceCandidates(roomId, socket.id, candidate, type);
+      }
+    );
+
+    socket.on("disconnect", () => {
+      this.removeUser(socket.id);
     });
   }
 }
